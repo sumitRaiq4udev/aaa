45e790ac3d7b240204e53a09582c1590d0b07855
sumit261997 <sumitrai@quantum4u.in>
2022-05-26 16:21:04 +0530

    Initial commit

---
+++ app/src/main/java/io/virtualapp/XApp.java
@@ -0,0 +1,2 @@
+package io.virtualapp;public class XApp {
+}
---
+++ app/src/main/java/io/virtualapp/delegate/MyVirtualInitializer.java
@@ -0,0 +1,9 @@
+package io.virtualapp;
+
+import com.lody.virtual.client.core.VirtualCore;
+
+public class MyVirtualInitializer extends VirtualCore.VirtualInitializer {
+    public MyVirtualInitializer(XApp xApp, VirtualCore virtualCore) {
+        super();
+    }
+}
